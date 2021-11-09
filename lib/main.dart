1542bceb73544e1c444b7b93878f925e5e7109f6
Prathambhandari <prathamkrishnabhandari17@gmail.com>
2021-11-09 15:48:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'hello',
+          ),
+        ),
+      ),
+    );
+  }
+}
